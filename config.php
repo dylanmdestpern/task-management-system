f59f5a1e738957b46394a1588e3de021d4393f49
Dylan de St Pern <dylanmdestpern@gmail.com>
2018-06-10 23:43:35 +0200

    This should not commit
    This change to config.php should be ignored.

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
     define('DEBUG_MODE', true); //Set to false for release
 
-	define('DB_PREFIX', 'devtaskmanager_');
+	define('DB_PREFIXs', 'devtaskmanager_');
 	
     define('DB_SERVER', 'localhost');
     define('DB_USER', 'dylandestpern');
